6eb01bc249b35cc2506238c4ca4337ec0890f6dc
abdal-security-group <abdal.group@gmail.comgit>
2023-10-15 13:41:00 +0330

    Abdal FileWatcher

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace FileWatcherExample
+{
+    class Program
+    {
+        private static List<FileSystemWatcher> watchers = new List<FileSystemWatcher>();
+
+        static void Main()
+        {
+            var banner = @"
+
+                                
+░█████╗░██████╗░██████╗░░█████╗░██╗░░░░░
+██╔══██╗██╔══██╗██╔══██╗██╔══██╗██║░░░░░
+███████║██████╦╝██║░░██║███████║██║░░░░░
+██╔══██║██╔══██╗██║░░██║██╔══██║██║░░░░░
+██║░░██║██████╦╝██████╔╝██║░░██║███████╗
+╚═╝░░╚═╝╚═════╝░╚═════╝░╚═╝░░╚═╝╚══════╝
+
+███████╗██╗██╗░░░░░███████╗░██╗░░░░░░░██╗░█████╗░████████╗░█████╗░██╗░░██╗███████╗██████╗░
+██╔════╝██║██║░░░░░██╔════╝░██║░░██╗░░██║██╔══██╗╚══██╔══╝██╔══██╗██║░░██║██╔════╝██╔══██╗
+█████╗░░██║██║░░░░░█████╗░░░╚██╗████╗██╔╝███████║░░░██║░░░██║░░╚═╝███████║█████╗░░██████╔╝
+██╔══╝░░██║██║░░░░░██╔══╝░░░░████╔═████║░██╔══██║░░░██║░░░██║░░██╗██╔══██║██╔══╝░░██╔══██╗
+██║░░░░░██║███████╗███████╗░░╚██╔╝░╚██╔╝░██║░░██║░░░██║░░░╚█████╔╝██║░░██║███████╗██║░░██║
+╚═╝░░░░░╚═╝╚══════╝╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝
+----------------------------------------------------
+Programmer: Ebrahim Shafiei (EbraSha)
+Email: Prof.Shafiei@Gmail.com
+----------------------------------------------------
+
+                ";
+          
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Version version = Assembly.GetExecutingAssembly().GetName().Version;
+
+            Console.Title = "Abdal FileWatcher " + version.Major + "." + version.Minor;
+
+            Console.WriteLine(banner);
+
+            // Get a list of all available drives
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in allDrives)
+            {
+                // Check if the drive is ready
+                if (!drive.IsReady)
+                {
+                    Console.WriteLine($"Drive {drive.Name} is not ready. Skipping...");
+                    continue;
+                }
+
+                // Create a FileSystemWatcher for each drive
+                var watcher = new FileSystemWatcher
+                {
+                    Path = drive.Name,
+                    NotifyFilter = NotifyFilters.FileName | NotifyFilters.DirectoryName,
+                    IncludeSubdirectories = true // Monitor subdirectories as well
+                };
+
+                watcher.Created += OnChanged;
+                watcher.EnableRaisingEvents = true;
+
+                watchers.Add(watcher);
+            }
+
+            Console.WriteLine("Monitoring all drives. Press 'q' to quit.");
+            while (Console.Read() != 'q') ;
+
+            // Dispose resources once monitoring is done
+            foreach (var watcher in watchers)
+            {
+                watcher.Dispose();
+            }
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Change detected: {e.FullPath}"+"\n");
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# Abdal-FileWatcher
-"Abdal FileWatcher" by Ebrahim Shafiei provides comprehensive file monitoring capabilities on Windows platforms, being open-source.
+# Abdal FileWatcher
+
+![](https://raw.githubusercontent.com/ebrasha/Abdal-Eni-Benchmark/main/Abdal-Eni-Benchmark.jpg)
+
+# 🤵 Programmer
+Ebrahim Shafiei (EbraSha)
+
+Email :  Prof.Shafiei@Gmail.com
+
+
+## 🤞 Cooperation
+If you, like me, are passionate about open-source projects and helping people worldwide, please reach out so we can collaborate to build a better world.
+
+
+## README Translation
+- [English](README.md)
+- [فارسی](README.fa.md)
+
+## 💎 General purpose
+"Abdal FileWatcher" by Ebrahim Shafiei: A cross-platform, open-source file monitoring tool tailored for security experts.
+
+## 📄 Description
+"Abdal FileWatcher" is a software designed for monitoring files across all operating systems. Developed by Ebrahim Shafiei, it's released as a fully open-source tool. Security professionals can utilize this application to observe all file changes in real-time. Its features encompass monitoring all drives and subdirectories, instant change notifications, and compatibility with every OS.
+
+# ✨ Features
+
+- Without any spyware or malicious code
+- Monitoring all drives
+- Very high speed
+- Suitable for ethical hacking and reverse engineering
+- Fully open-source
+- Multi-platform
+- Simple and uncomplicated
+
+
+# 💡 Installation
+
+#### For all operating systems
+Just download and run the version you desire.
+ 
+## ✅ Supported operating systems
+
+- Win 10 x64
+- debian.11-x64
+- debian.11-arm64
+- debian.10-x64
+- ubuntu.22.04-x64
+- ubuntu.22.04-arm64
+- ubuntu.20.04-x64
+- ubuntu.18.04-x64
+- centos.7-x64
+- rhel.7-x64
+- rhel.8-x64
+- fedora.36-x64
+- fedora.37-x64
+- osx-x64
+
+ 
+
+## ⚠️ Legal disclaimer ⚠️
+
+Use this tool only for legal purposes and with permission. The developer assumes no liability for any illicit usage or harm inflicted upon others as a result of using this tool.
+
+## ❤️ Donations to the project
+
+> USDT:      TKRmTvwkMFdgGrwvDHztLXsKAwK2WEEKUp
+
+> bitcoin:   bc1q9w9ymgz2wluax60rsuza4q0at7gpy82g8el6zj
+
+> For Iranian People -> MellatBank : 6104-3378-5301-4247
+
+## ❗ Reporting Issues
+
+If you are facing a configuration issue or something is not working as you expected to be, please use the Prof.Shafiei@Gmail.com . Issues on GitLab are also welcomed.
+
